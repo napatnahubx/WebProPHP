0d89412e9e4b3678b3bb4d3e174fd474d9b58a27
napat sangthongthat <napat@git.com>
2021-01-13 11:06:30 +0700

    update

---
+++ Lab4-1.php
@@ -0,0 +1,21 @@
+<html>
+<head><title>การก าหนดค่าตัวแปร</title></head>
+<body>
+<?php
+$x = 100;
+$y = "200";
+$z = '300';
+$add = $x + $y;
+$sub = $z - $y;
+$mul= $z * $y;
+$div=$mul/$z;
+$message = "Hello World";
+
+echo $add; echo "<br />";
+echo $sub; echo "<br />";
+echo $mul; echo "<br />";
+echo $div; echo "<br />";
+
+?>
+</body>
+</html>
