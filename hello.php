cb185a00ae298b9b76f9198f7e85f24f07d99a8b
napat sangthongthat <napat@git.com>
2021-01-13 10:15:52 +0700

    update

---
+++ hello.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>hello</title>
+        </head>
+        <body>
+        <?php
+echo"<h1> Hello</h1><br>";
+echo"<h2>Pathum Thani<h2>";
+        ?>
+       </body>
+</html>
