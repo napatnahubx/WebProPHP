9c5325214c9a63c1475887761e493d1ba82b5b49
napat sangthongthat <napat@git.com>
2021-01-20 08:48:51 +0700

    update

---
+++ Lab4-5.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>การใช้ Single Quote('') และ Double Quote("")</title>
+</head>
+<body>
+<font size="4">
+<?php
+$name = "สมชาย";
+$age = 30;
+$country = "ประเทศไทย";
+echo "<b>Double Quote:</b> My name is $name, $age years old from$country";
+?>
+</font>
+</body>
+</html>
